edf4fc95f2918aa0de88414c0e4fc2b4ced81669
Abdul Ragib <abdulragib36@gmail.com>
2022-12-29 18:34:25 +0530

    Solution 2.0

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-//your code here
-const show = document.getElementById('wordCount')
-const box = document.getElementById('evaluatedText')
+// Get the textbox and h3 elements
+const textbox = document.getElementById('evaluatedText');
+const wordCount = document.getElementById('wordCount');
 
-box.addEventListener('keyup', function(){
-    let count = box.value.split(' ').length
-    show.innerHTML = count
-})
+// Add an event listener to the textbox that listens for changes
+textbox.addEventListener('input', function() {
+    
+    if(evaluatedText.value === ""){
+        wordCount.innerText = 0;
+    }
+    else{
+        // Get the value of the textbox
+        const text = textbox.value;
+
+        // Split the text into an array of words
+        const words = text.trim().split(' ');
+
+        // Set the inner HTML of the h3 to the number of words
+        wordCount.innerHTML = words.length;
+    }
+ 
+});
