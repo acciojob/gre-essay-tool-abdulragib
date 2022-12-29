84a1042a2900f98f8fbcb6bc02462b6849e0a3ed
Abdul Ragib <abdulragib36@gmail.com>
2022-12-29 18:11:09 +0530

    Solution 1.0

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+const show = document.getElementById('wordCount')
+const box = document.getElementById('evaluatedText')
+
+box.addEventListener('keyup', function(){
+    let count = box.value.split(' ').length
+    show.innerHTML = count
+})
