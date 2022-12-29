2af1322397a597dd0d1af300d3527f3ee05e9c09
Abdul Ragib <abdulragib36@gmail.com>
2022-12-29 18:41:11 +0530

    Solution 3.0

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 // Get the textbox and h3 elements
-const textbox = document.getElementById('evaluatedText');
+const textBox = document.getElementById('evaluatedText');
 const wordCount = document.getElementById('wordCount');
 
 // Add an event listener to the textbox that listens for changes
-textbox.addEventListener('input', function() {
+textBox.addEventListener('input', function() {
     
-    if(evaluatedText.value === ""){
+    if(textBox.value === ""){
         wordCount.innerText = 0;
     }
     else{
-        // Get the value of the textbox
-        const text = textbox.value;
+        const text = textBox.value.trim().split(' ');
 
-        // Split the text into an array of words
-        const words = text.trim().split(' ');
-
-        // Set the inner HTML of the h3 to the number of words
-        wordCount.innerHTML = words.length;
+        if(text.length>0)
+          {
+            wordCount.innerHTML = text.length;
+          }
     }
  
 });
